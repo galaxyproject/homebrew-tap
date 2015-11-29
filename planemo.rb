503c05095304e4667c9f94e136a9ea402e584ef2
John Chilton <jmchilton@gmail.com>
2015-11-29 10:55:31 +0000

    Rev planemo to version 0.21.0

--- planemo.rb
+++ planemo.rb
@@ -7,8 +7,8 @@ require "formula"
 
 class Planemo < Formula
   homepage "http://planemo.readthedocs.org/en/latest/"
-  url "https://pypi.python.org/packages/source/p/planemo/planemo-0.20.0.tar.gz"
-  sha256 "3ba1c34038462c1d00bf5223e7e0046af150b3d09fa34b82c413a72475de6dfe"
+  url "https://pypi.python.org/packages/source/p/planemo/planemo-0.21.0.tar.gz"
+  sha256 "0c24fd87c2f4358b7b2d2f19e2bbbc2c8de4a15cb46aa9c680b4393b693d5e0c"
   head "https://github.com/galaxyproject/planemo.git"
 
   option "without-completions", "Disable bash/zsh completions"
